431d244bf22c6eafd6c4475f16a21abfeedcf787
Evgeny Panchenko <worldbatle@mail.ru>
2025-03-03 21:11:23 +0300

    Внесены изменения: 1. Убран повторяющийся текст из условного оператора. 2. Убран повторный вызов функции проверки сущестования треуголника, используется ранее созданная переменная. 3. Фунцкции переименованны, в соответствии с правилами хорошего кода (ранее так назвал, так как они в условии задачи так именовались) 4. Так как теперь используется переменная, сделал пряморукое условие

--- HW-2/main.cpp
+++ HW-2/main.cpp
@@ -2,7 +2,7 @@
 #include <cmath> // Для sqrt, pow
 
 // Функция f(x1, y1, x2, y2)
-double f(double x1, double y1, double x2, double y2)
+double calculateDistance(double x1, double y1, double x2, double y2)
 {
     // Используем формулу расстояния
     double dx = x2 - x1;
@@ -11,7 +11,7 @@ double f(double x1, double y1, double x2, double y2)
 }
 
 // Функция t(a, b, c)
-bool t(double a, double b, double c)
+bool canFormTriangle(double a, double b, double c)
 {
     return ((a + b > c) && (b + c > a) && (a + c > b)) ? true : false;
 }
@@ -23,12 +23,12 @@ int main()
     std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
 
     // Вычисляем длины сторон
-    double a = f(x1, y1, x2, y2);
-    double b = f(x2, y2, x3, y3);
-    double c = f(x3, y3, x1, y1);
+    double a = calculateDistance(x1, y1, x2, y2);
+    double b = calculateDistance(x2, y2, x3, y3);
+    double c = calculateDistance(x3, y3, x1, y1);
 
     // Проверяем, можно ли построить треугольник
-    bool canExist = t(a, b, c);
+    bool canExist = canFormTriangle(a, b, c);
 
     // Вывод результатов
 
@@ -36,7 +36,7 @@ int main()
     std::cout << "Длина стороны b = " << b << std::endl;
     std::cout << "Длина стороны c = " << c << std::endl;
 
-    std::cout << (t(a, b, c) ? "Треугольник с такими сторонами существует." : "Треугольник с такими сторонами не существует.");
+    std::cout << "Треугольник с такими сторонами " << (canExist ? "существует." : "не существует.") << std::endl;
 
 	return 0;
 }
